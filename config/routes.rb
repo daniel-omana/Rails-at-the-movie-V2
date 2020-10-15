dc3236eae7201dfdd176c736270d72f424e35350
Daniel <danielomana@shaw.ca>
2020-10-15 17:01:11 -0500

    Added a generated Home controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'home/index'
   resources :movies, only: %i[index show]
   resources :production_companies, only: %i[index show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
