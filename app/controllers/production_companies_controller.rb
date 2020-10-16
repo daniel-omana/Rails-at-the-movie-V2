c87c759b176e21e5cd9eb1b241855f1a9369bd65
Daniel <danielomana@shaw.ca>
2020-10-16 02:20:01 -0500

    Now ordering production companies by the number of movies they made.

--- app/controllers/production_companies_controller.rb
+++ app/controllers/production_companies_controller.rb
@@ -1,7 +1,13 @@
 class ProductionCompaniesController < ApplicationController
   def index
-    @production_companies = ProductionCompany.all
+    @production_companies = ProductionCompany.ordered_by_movies
   end
+
+  # SELECT COUNT(production_companies.id), production_companies.id, production_companies.name
+  # FROM "production_companies"
+  # LEFT OUTER JOIN "movies"
+  # 	ON "movies"."production_company_id" = "production_companies"."id"
+  # GROUP BY production_companies.id;
 
   def show
     @production_company = ProductionCompany.find(params[:id])
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
   resources :movies, only: %i[index show]
   resources :production_companies, only: %i[index show]
+
+  root to: 'home#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
